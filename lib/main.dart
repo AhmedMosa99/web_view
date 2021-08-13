cb93293a1aadf2e4d8a0722d7c6dc896745def4c
unknown <houda.mosa.2020@gmail.com>
2021-08-13 12:02:59 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'GoogleDrive'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    void initState() {
+      super.initState();
+      // Enable hybrid composition.
+
+      if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://drive.google.com/drive/my-drive',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
